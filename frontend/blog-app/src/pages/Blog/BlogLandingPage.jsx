169c242812cdc726f9512a4c788dea14d9291c6a
saba871 <sabatabatadze40@gmail.com>
2026-03-21 11:43:45 -0700

    last commit

--- frontend/blog-app/src/pages/Blog/BlogLandingPage.jsx
+++ frontend/blog-app/src/pages/Blog/BlogLandingPage.jsx
@@ -3,123 +3,78 @@ import BlogLayout from '../../components/Layouts/BlogLayout/BlogLayout'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPath'
 import { useNavigate } from 'react-router-dom'
-import moment from "moment"
-import { LuGalleryVerticalEnd, LuLoaderCircle } from "react-icons/lu"
+import moment from 'moment'
+import { LuGalleryVerticalEnd, LuLoaderCircle } from 'react-icons/lu'
 import FeatuedBlogPost from './components/FeatuedBlogPost'
 import BlogPostSummaryCard from './components/BlogPostSummaryCard'
 import TrendingPostSection from './components/TrendingPostSection'
 
 const BlogLandingPage = () => {
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
-    const [blogPostList, setBlogPostList] = useState([])
-    const [page, setPage] = useState(1)
-    const [totalPages, setTotalPages] = useState(null)
-    const [loading, setIsLoading] = useState(false)
+	const [blogPostList, setBlogPostList] = useState([])
+	const [page, setPage] = useState(1)
+	const [totalPages, setTotalPages] = useState(null)
+	const [loading, setIsLoading] = useState(false)
 
-    // fetch all posts
-    const getAllPosts = async (pageNumber = 1) => {
-        try {
-            setIsLoading(true)
-            const response = await axiosInstance.get(API_PATHS.POSTS.GET_ALL, {
-                params: {
-                    status: "published",
-                    page: pageNumber
-                }
-            })
+	// fetch all posts
+	const getAllPosts = async (pageNumber = 1) => {
+		try {
+			setIsLoading(true)
+			const response = await axiosInstance.get(API_PATHS.POSTS.GET_ALL, {
+				params: {
+					status: 'published',
+					page: pageNumber,
+				},
+			})
 
-            const {posts, totalPages} = response.data;
-            setBlogPostList((prevPost) => pageNumber == 1 ? posts : [...prevPost, ...posts])
-            setTotalPages(totalPages)
-            setPage(pageNumber)
-        } catch (error) {
-            console.error("Error fetching data: ", error)
-        } finally {
-            setIsLoading(false)
-        }
-    }
+			const { posts, totalPages } = response.data
+			setBlogPostList(prevPost => (pageNumber == 1 ? posts : [...prevPost, ...posts]))
+			setTotalPages(totalPages)
+			setPage(pageNumber)
+		} catch (error) {
+			console.error('Error fetching data: ', error)
+		} finally {
+			setIsLoading(false)
+		}
+	}
 
-    // load more posts
-    const handleMore = () => {
-        if (page < totalPages) {
-            getAllPosts(page + 1)
-        }
-    }
+	// load more posts
+	const handleMore = () => {
+		if (page < totalPages) {
+			getAllPosts(page + 1)
+		}
+	}
 
-    useEffect(() => {
-        getAllPosts(1)
-    }, [])
+	useEffect(() => {
+		getAllPosts(1)
+	}, [])
 
-    const handleClick = (post) => {
-        navigate(`/${post.slug}`)
-    }
-    return (
-      <BlogLayout activeMenu="01">
-        <div className="grid grid-cols-12 gap-5">
-          <div className="col-span-12 md:col-span-9">
-            {blogPostList.length > 0 && (
-              <FeatuedBlogPost
-                title={blogPostList[0].title}
-                coverImageUrl={blogPostList[0].coverImageUrl}
-                description={blogPostList[0].content}
-                tags={blogPostList[0].tags}
-                updatedOn={
-                  blogPostList[0].updatedAt
-                    ? moment(blogPostList[0].updatedAt).format('Do MMM yyyy')
-                    : '-'
-                }
-                authorName={blogPostList[0].authorName}
-                authProfileImg={blogPostList[0].author.profileImageUrl}
-                onClick={() => handleClick(blogPostList[0])}
-              />
-            )}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-              {blogPostList.length > 0 &&
-                blogPostList
-                  .slice(1)
-                  .map((item) => (
-                    <BlogPostSummaryCard
-                      key={item._id}
-                      title={item.title}
-                      coverImageUrl={item.coverImageUrl}
-                      description={item.content}
-                      tags={item.tags}
-                      updatedOn={
-                        item.updatedAt
-                          ? moment(item.updatedAt).format('Do MMM YYYY')
-                          : '-'
-                      }
-                      authorName={item.author.name}
-                      authProfileImg={item.author.profileImageUrl}
-                      onClick={() => handleClick(item)}
-                    />
-                  ))}
-            </div>
+	const handleClick = post => {
+		navigate(`/${post.slug}`)
+	}
+	return (
+		<BlogLayout activeMenu="01">
+			<div className="grid grid-cols-12 gap-5">
+				<div className="col-span-12 md:col-span-9">
+					{blogPostList.length > 0 && <FeatuedBlogPost title={blogPostList[0].title} coverImageUrl={blogPostList[0].coverImageUrl} description={blogPostList[0].content} tags={blogPostList[0].tags} updatedOn={blogPostList[0].updatedAt ? moment(blogPostList[0].updatedAt).format('Do MMM yyyy') : '-'} authorName={blogPostList[0].authorName} authProfileImg={blogPostList[0].author.profileImageUrl} onClick={() => handleClick(blogPostList[0])} />}
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">{blogPostList.length > 0 && blogPostList.slice(1).map(item => <BlogPostSummaryCard key={item._id} title={item.title} coverImageUrl={item.coverImageUrl} description={item.content} tags={item.tags} updatedOn={item.updatedAt ? moment(item.updatedAt).format('Do MMM YYYY') : '-'} authorName={item.author.name} authProfileImg={item.author.profileImageUrl} onClick={() => handleClick(item)} />)}</div>
 
-            {page < totalPages && (
-              <div className="flex items-center justify-center mt-5">
-                <button
-                  className="flex items-center gap-3 text-sm text-white font-medium bg-black px-7 py-2.5 mt-6 rounded-full text-nowrap hover:scale-105 transition-all cursor-pointer"
-                  disabled={loading}
-                  onClick={handleMore}
-                >
-                  {loading ? (
-                    <LuLoaderCircle className="animate-spin text-[15px]" />
-                  ) : (
-                    <LuGalleryVerticalEnd className="text-lg" />
-                  )}{' '}
-                  {loading ? 'Loading...' : 'Load More'}
-                </button>
-              </div>
-            )}
-          </div>
+					{page < totalPages && (
+						<div className="flex items-center justify-center mt-5">
+							<button className="flex items-center gap-3 text-sm text-white font-medium bg-black px-7 py-2.5 mt-6 rounded-full text-nowrap hover:scale-105 transition-all cursor-pointer" disabled={loading} onClick={handleMore}>
+								{loading ? <LuLoaderCircle className="animate-spin text-[15px]" /> : <LuGalleryVerticalEnd className="text-lg" />} {loading ? 'Loading...' : 'Load More'}
+							</button>
+						</div>
+					)}
+				</div>
 
-          <div className="col-span-12 md:col-span-3">
-            <TrendingPostSection />
-          </div>
-        </div>
-      </BlogLayout>
-    );
+				<div className="col-span-12 md:col-span-3">
+					<TrendingPostSection />
+				</div>
+			</div>
+		</BlogLayout>
+	)
 }
 
 export default BlogLandingPage
